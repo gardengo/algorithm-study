1292c2dbe4e8dbbd2ebc64948e65608261c9f595
GardenGo <7704wjddnjs@gmail.com>
2022-11-20 19:55:32 +0900

    컴파일 에러 수정

--- Inflearn/Section2/Q1.java
+++ Inflearn/Section2/Q1.java
@@ -7,7 +7,7 @@ public class Q1 { //큰 수 출력하기
     public ArrayList<Integer> solution(int n, int[] arr) {
         ArrayList<Integer> answer = new ArrayList<>();
         answer.add(arr[0]);
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
             if (arr[i] > arr[i - 1]) answer.add(arr[i]);
         return answer;
     }
@@ -18,6 +18,6 @@ public class Q1 { //큰 수 출력하기
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
-        for (int x : T.solution(n, arr)) System.out.println(x + " ");
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
     }
 }
