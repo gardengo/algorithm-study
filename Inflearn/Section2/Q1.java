a7d9e12390e794755365742a3441e4b7da9feb3a
GardenGo <7704wjddnjs@gmail.com>
2022-11-27 19:53:58 +0900

    Q1. 큰 수 출력하기

--- Inflearn/Section2/Q1.java
+++ Inflearn/Section2/Q1.java
@@ -7,7 +7,7 @@ public class Q1 { //큰 수 출력하기
     public ArrayList<Integer> solution(int n, int[] arr) {
         ArrayList<Integer> answer = new ArrayList<>();
         answer.add(arr[0]);
-        for (int i = 1; i < n; i++)
+        for (int i = 0; i < n; i++)
             if (arr[i] > arr[i - 1]) answer.add(arr[i]);
         return answer;
     }
