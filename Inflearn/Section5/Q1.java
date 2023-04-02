b6dea44de81dee3262171d6ceb9642ac1420b3bd
GardenGo <7704wjddnjs@gmail.com>
2023-04-02 17:57:55 +0900

    Q1. 올바른 괄호
    괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
    (())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.

---
+++ Inflearn/Section5/Q1.java
@@ -0,0 +1,27 @@
+package Section5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Q1 { //올바른 괄호
+    public String solution(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == '(') stack.push(x);
+            else {
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+        if (!stack.isEmpty()) return "NO";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q1 T = new Q1();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.print(T.solution(str));
+    }
+}
