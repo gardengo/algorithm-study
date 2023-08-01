c9ee1999d55f16fbcf6aa539a6c68b473862cd1f
GardenGo <7704wjddnjs@gmail.com>
2023-08-01 11:10:00 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 356 ms, Memory: 90212 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
 	static int N;
@@ -9,23 +10,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 
-		int K = (int) (Math.pow(2, N) - 1);
+		BigInteger K = new BigInteger("2");
+		K = K.pow(N).subtract(BigInteger.ONE);
 		sb.append(K + "\n");
-		hanoi(N, 1, 2, 3);
+		if (N <= 20)
+			hanoi(N, 1, 2, 3);
 
 		System.out.println(sb);
 	}
 
 	public static void hanoi(int n, int start, int tmp, int end) {
 		if (n == 1) {
-			if (N <= 20)
-				sb.append(start + " " + end + "\n");
+			sb.append(start + " " + end + "\n");
 			return;
 		}
 
 		hanoi(n - 1, start, end, tmp);
-		if (N <= 20)
-			sb.append(start + " " + end + "\n");
+		sb.append(start + " " + end + "\n");
 		hanoi(n - 1, tmp, start, end);
 	}
 
