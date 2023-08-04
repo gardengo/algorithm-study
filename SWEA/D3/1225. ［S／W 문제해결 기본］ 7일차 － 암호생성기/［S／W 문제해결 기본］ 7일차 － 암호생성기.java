a7c308483150bdbd8b4cacdf581d78d1dace254a
GardenGo <7704wjddnjs@gmail.com>
2023-08-04 12:46:55 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 134 ms, Memory: 26,332 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -13,17 +13,19 @@ public class Solution {
 		for (int test_case = 1; test_case <= 10; test_case++) { // 테스트 케이스 만큼 반복
 			int n = Integer.parseInt(br.readLine());
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			List<Integer> list = new ArrayList<Integer>();
+			Queue<Integer> queue = new LinkedList<Integer>();
 			for (int i = 0; i < 8; i++)
-				list.add(Integer.parseInt(st.nextToken()));
+				queue.add(Integer.parseInt(st.nextToken()));
 
 			int num = 1;
-			while (list.get(7) > 0) {
-				if (list.get(0) - num < 0)
-					list.add(0);
-				else
-					list.add(list.get(0) - num);
-				list.remove(0);
+			while (true) { // 마지막 자리가 0이되면 멈춤
+				if (queue.peek() - num <= 0) { // num를 뺐을 때 음수가 되면 0을 추가
+					queue.remove();
+					queue.add(0);
+					break;
+				} else {
+					queue.add(queue.poll() - num); // 첫 번째 수에서 num를 빼서 뒤에 추가
+				}
 				num++;
 				if (num > 5)
 					num = 1;
@@ -33,7 +35,7 @@ public class Solution {
 			sb.append(test_case);
 			for (int i = 0; i < 8; i++) {
 				sb.append(" ");
-				sb.append(list.get(i));
+				sb.append(queue.poll());
 			}
 			sb.append("\n");
 		}
