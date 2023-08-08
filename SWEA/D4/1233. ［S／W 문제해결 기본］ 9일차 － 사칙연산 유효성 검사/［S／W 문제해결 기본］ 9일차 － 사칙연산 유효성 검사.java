1ebd33b50b59939aeee0d0bec6febde783b49736
GardenGo <7704wjddnjs@gmail.com>
2023-08-08 13:54:25 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 117 ms, Memory: 21,200 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 메모리 20,168kb 실행시간 136ms
 public class Solution {
 
 	public static void main(String[] args) throws Exception {
@@ -21,7 +22,14 @@ public class Solution {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				st.nextToken();
 				String str = st.nextToken();
-				if (!(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/"))) {
+				if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
+					if (!st.hasMoreTokens()) {
+						answer = 0;
+					}
+					while (st.hasMoreTokens()) {
+						st.nextToken();
+					}
+				} else {
 					if (st.hasMoreTokens()) {
 						answer = 0;
 					}
