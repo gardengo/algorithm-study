94f683a0840992d52351f271b381e69cb7ae9367
GardenGo <7704wjddnjs@gmail.com>
2023-08-08 15:20:23 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 151 ms, Memory: 22,812 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 메모리 95,880kb 실행시간 489kb
 public class Solution {
 
 	public static void main(String[] args) throws Exception {
@@ -14,28 +13,31 @@ public class Solution {
 
 		for (int test_case = 1; test_case <= T; test_case++) { // 테스트 케이스 만큼 반복
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int[] a = new int[N];
+			int N = Integer.parseInt(st.nextToken()); // 과자 봉지의 개수
+			int M = Integer.parseInt(st.nextToken()); // 무게 합 제한
+			int[] a = new int[N]; // 과자 봉지의 무게
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++)
 				a[i] = Integer.parseInt(st.nextToken());
 
-			Set<Integer> set = new HashSet<Integer>();
-			for (int i = 0; i < N - 1; i++) {
-				for (int j = i + 1; j < N; j++) {
-					if (a[i] + a[j] <= M)
-						set.add(a[i] + a[j]);
+			int max = -1;
+			Arrays.sort(a);
+			int lt = 0;
+			int rt = N - 1;
+			while (lt < rt) {
+				int sum = a[lt] + a[rt];
+				if (sum > M) {
+					rt--;
+				} else if (sum == M) {
+					max = sum;
+					break;
+				} else {
+					max = Math.max(max, sum);
+					lt++;
 				}
 			}
-			sb.append("#");
-			sb.append(test_case);
-			sb.append(" ");
-			if (set.size() > 0)
-				sb.append(Collections.max(set));
-			else
-				sb.append(-1);
-			sb.append("\n");
+
+			sb.append("#").append(test_case).append(" ").append(max).append("\n");
 		}
 		System.out.println(sb);
 	}
