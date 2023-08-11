ece1de728f68bfaf3d380e4658d85de400a621f3
GardenGo <7704wjddnjs@gmail.com>
2023-08-11 12:50:51 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+// 메모리 11484kb 시간 80ms
 public class Main { // 클래스 시작
 	private static boolean[] visited = new boolean[9]; // 조합을 만들기 위해 방문한 지점을 표시
 	private static int sum; // 모자에 적힌 숫자의 합을 계산하기위한 변수
@@ -18,7 +19,7 @@ public class Main { // 클래스 시작
 		}
 
 		combination(0, 0, 9, 2); // 조합 시작
-		for (int i = answer.size() - 7; i < answer.size(); i++) // 일곱 난쟁이의 숫자를 순서대로
+		for (int i = 0; i < 7; i++) // 일곱 난쟁이의 숫자를 순서대로
 			System.out.println(answer.get(i)); // println으로 출력한다
 	} // 메인 종료
 
@@ -35,7 +36,7 @@ public class Main { // 클래스 시작
 		for (int i = start; i < n; i++) { // 0부터 9까까지 돌면서 조합한다
 			visited[i] = true; // 방문한 모자는 표시하고
 			calc += hat[i]; // 그 숫자를 더해준다
-			combination(calc, start + 1, n, r - 1); // 더한 숫자 다음부터 탐색한다
+			combination(calc, i + 1, n, r - 1); // 더한 숫자 다음부터 탐색한다
 			visited[i] = false; // 재귀가 풀리면 다시 false로 바꾸고
 			calc -= hat[i]; // 더한 숫자로 빼준다
 		}
