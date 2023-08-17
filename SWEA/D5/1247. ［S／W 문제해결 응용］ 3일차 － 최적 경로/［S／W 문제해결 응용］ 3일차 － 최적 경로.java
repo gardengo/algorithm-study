01b1e75ff7b9b21d0acfddbdf0ed1873797c4c3a
GardenGo <7704wjddnjs@gmail.com>
2023-08-17 13:59:22 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 375 ms, Memory: 20,384 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -29,33 +29,35 @@ public class Solution { // 클래스시작
 			} // 입력 종료
 
 			// 회사에서 출발 -> 고객의 집을 모두 방문하고 -> 집으로 돌아온다
-			answer = 2000;
+			answer = 2000; // 정답 초기값 설정
 			visited = new boolean[N]; // 방문한 고객의 집을 표시한 배열
-			permutation(office[0], office[1], 0, 0);
+			permutation(office[0], office[1], 0, 0); // 회사 위치를 시작으로 순열을 만든다
 
 			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
 		} // 테스트 케이스 종료
 		System.out.println(sb);
 	}// 메인 종료
 
-	static void permutation(int nx, int ny, int sum, int depth) {
-		if (depth == N) {
-			sum += Math.abs(house[0] - nx) + Math.abs(house[1] - ny);
-			if (answer > sum)
+	static void permutation(int nx, int ny, int sum, int depth) { // 현재의 x, 현재의 y, 현재까지 합, 반복횟수
+		if (sum > answer) // 현재까지의 합이 정답보다 크면
+			return; // 더이상 탐색하지 않고 멈춘다
+		if (depth == N) { // 고객을 모두 방문하면
+			sum += Math.abs(house[0] - nx) + Math.abs(house[1] - ny); // 집까지의 거리를 더하고
+			if (answer > sum) // 정답과 비교한다
 				answer = sum;
-			return;
+			return; // 재귀 종료
 		}
-		for (int i = 0; i < N; i++) {
-			if (nx == customer[i][0] && ny == customer[i][1])
+		for (int i = 0; i < N; i++) { // 고객의 집을 모두 방문
+			if (nx == customer[i][0] && ny == customer[i][1]) // 현재의 위치는 건너뛴다
 				continue;
-			if (!visited[i]) {
-				sum += Math.abs(customer[i][0] - nx) + Math.abs(customer[i][1] - ny);
-				visited[i] = true;
-				permutation(customer[i][0], customer[i][1], sum, depth + 1);
-				visited[i] = false;
-				sum -= Math.abs(customer[i][0] - nx) + Math.abs(customer[i][1] - ny);
+			if (!visited[i]) { // 방문한 적이 없다면
+				sum += Math.abs(customer[i][0] - nx) + Math.abs(customer[i][1] - ny); // 거리를 구해서 더한다
+				visited[i] = true; // 방문 표시
+				permutation(customer[i][0], customer[i][1], sum, depth + 1); // 현재 위치를 바꾸고 다음 탐색
+				visited[i] = false; // 재귀가 끝나면 false로
+				sum -= Math.abs(customer[i][0] - nx) + Math.abs(customer[i][1] - ny); // 거리도 다시 빼준다
 			}
 		}
-	}
+	} // 순열 종료
 
 } // 클래스 종료
