f3ceefdcbb17f158a0a732502da7a4c025ec1c73
GardenGo <7704wjddnjs@gmail.com>
2023-08-24 13:48:00 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,309 ms, Memory: 119,068 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,18 +1,42 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution { // 클래스 시작
+	static int parents[];
 
-	static class Node {
-		int no, cost;
+	private static void make(int n) { // 집합을 만드는 메소드
+		parents = new int[n + 1]; // n크기의 집합을 만든다
+		for (int i = 1; i <= n; i++) // 초기값 설정
+			parents[i] = i; // 자기 자신을 가르키게 설정한다
+	}
 
-		public Node(int no, int cost) {
+	private static int find(int n) { // 원소를 찾는 메소드
+		if (parents[n] == n) // 부모와 자신이 같으면
+			return n; // 거기가 루트이다
+		return parents[n] = find(parents[n]); // 루트를 찾을때까지 find 호출
+	}
+
+	private static boolean union(int a, int b) { // 집합을 합치는 메소드
+		int aRoot = find(a); // a루트를 찾고
+		int bRoot = find(b); // b루트를 찾는다
+
+		if (aRoot == bRoot) { // a루트와 b루트가 다르면
+			return false; // false를 리턴한다
+		} else { // a루트와 b루트가 같으면
+			parents[bRoot] = aRoot; // b루트를 a루트로 바꾼다
+			return true; // true를 리턴한다
+		}
+	}
+
+	static class Edge { // 엣지 클래스
+		int start, end, cost; // 시작정점, 종료정점, 비용
+
+		public Edge(int start, int end, int cost) {
 			super();
-			this.no = no;
+			this.start = start;
+			this.end = end;
 			this.cost = cost;
 		}
 	}
@@ -27,41 +51,22 @@ public class Solution { // 클래스 시작
 			int V = Integer.parseInt(st.nextToken()); // 정점의 개수 V
 			int E = Integer.parseInt(st.nextToken()); // 간선의 개수 E
 
-			ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>(); // 간선의 정보를 담은 그래프
-			for (int i = 0; i <= V; i++) // 1부터 V까지
-				graph.add(new ArrayList<Node>()); // 정점 추가
-
+			PriorityQueue<Edge> pq = new PriorityQueue<Edge>((a, b) -> Integer.compare(a.cost, b.cost)); // 비용으로 정렬하는 pq
 			for (int i = 0; i < E; i++) { // E개의 줄에서 간선의 정보를 받는다
 				st = new StringTokenizer(br.readLine().trim()); // 간선의 정보를 공백으로 나눈다
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				int cost = Integer.parseInt(st.nextToken());
-				graph.get(a).add(new Node(b, cost));
-				graph.get(b).add(new Node(a, cost));
+				int a = Integer.parseInt(st.nextToken()); // 시작 정점
+				int b = Integer.parseInt(st.nextToken()); // 종료 정점
+				int cost = Integer.parseInt(st.nextToken()); // 연결하는 비용
+				pq.offer(new Edge(a, b, cost)); // a와 b의 간선 큐에 추가
 			}
 
-			boolean[] visited = new boolean[V + 1];
-			int[] weight = new int[V + 1];
-			Arrays.fill(weight, Integer.MAX_VALUE);
-			long result = 0;
+			make(V);
+			long result = 0; // 전체 가중치의 합
 
-			PriorityQueue<Node> pq = new PriorityQueue<Node>((a, b) -> Integer.compare(a.cost, b.cost)); // 비용으로 정렬하는 pq
-			pq.offer(new Node(1, 0)); // 임의의 초기 정점 설정(1)
-			weight[1] = 0;
-
-			while (!pq.isEmpty()) {
-				Node cur = pq.poll();
-				if (visited[cur.no])
-					continue;
-				visited[cur.no] = true;
-				result += cur.cost;
-
-				for (int i = 0; i < graph.get(cur.no).size(); i++) {
-					Node next = graph.get(cur.no).get(i);
-					if (!visited[next.no] && next.cost < weight[next.no]) {
-						weight[next.no] = next.cost;
-						pq.offer(next);
-					}
+			while (!pq.isEmpty()) { // 큐가 비어있을 때까지 반복
+				Edge cur = pq.poll(); // 현재 노드
+				if (union(cur.start, cur.end)) {
+					result += cur.cost;
 				}
 			}
 			sb.append("#").append(test_case).append(" ").append(result).append("\n");
