c639d07852ccd05492a5b26570e5a0ed4acd5c83
GardenGo <7704wjddnjs@gmail.com>
2023-09-10 00:40:06 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 11852 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[] arr, dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		dp = new int[N];
+
+		func();
+		int answer = 0;
+		for (int i = 0; i < N; i++)
+			answer = dp[i] > answer ? dp[i] : answer;
+			
+		System.out.println(answer);
+	}
+
+	static void func() {
+		for (int i = 0; i < N; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i])
+					dp[i] = dp[j] + 1 > dp[i] ? dp[j] + 1 : dp[i];
+			}
+		}
+	}
+}
