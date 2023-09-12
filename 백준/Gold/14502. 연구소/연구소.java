5b87d34160e5348f43faa73cac31ba8f7afe82d3
GardenGo <7704wjddnjs@gmail.com>
2023-09-12 15:54:02 +0900

    [Gold IV] Title: 연구소, Time: 736 ms, Memory: 294324 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -11,7 +9,6 @@ public class Main {
 	static int[] dy = { 0, 0, -1, 1 };
 	static int N, M, safety, answer;
 	static int[][] map;
-	static List<int[]> birus;
 	static boolean[][] visited;
 
 	public static void main(String[] args) throws Exception {
@@ -21,7 +18,6 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N + 2][M + 2]; // IndexOutOfBounds를 벽으로 처리
 		safety = 0; // 최초에 주어진 안전영역
-		birus = new ArrayList<int[]>();
 
 		for (int i = 0; i < N + 2; i++) {
 			map[i][0] = 1;
@@ -37,15 +33,12 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == 0)
 					safety++;
-				if (map[i][j] == 2)
-					birus.add(new int[] { i, j });
 			}
 		}
 
-		visited = new boolean[N + 2][M + 2];
 		answer = 0;
 		comb(3);
-		System.out.println(answer);
+		System.out.println(answer - 3);
 	}
 
 	private static void comb(int cnt) {
@@ -57,35 +50,25 @@ public class Main {
 		for (int a = 1; a <= N; a++)
 			for (int b = 1; b <= M; b++)
 				if (map[a][b] == 0) {
-					if (visited[a][b])
-						continue;
-					visited[a][b] = true;
 					map[a][b] = 1;
-					safety--;
-
 					comb(cnt - 1);
-					visited[a][b] = false;
 					map[a][b] = 0;
-					safety++;
 				}
-
 	}
 
 	private static void bfs() {
-		int[][] copyMap = new int[N + 2][M + 2];
-		for (int i = 0; i < N + 2; i++)
-			for (int j = 0; j < M + 2; j++)
-				copyMap[i][j] = map[i][j];
-
 		int result = safety;
 		Queue<int[]> que = new ArrayDeque<int[]>();
-		for (int i = 0; i < birus.size(); i++)
-			que.offer(birus.get(i));
+		int[][] copyMap = new int[N + 2][M + 2];
+		for (int i = 0; i < N + 2; i++)
+			for (int j = 0; j < M + 2; j++) {
+				copyMap[i][j] = map[i][j];
+				if (map[i][j] == 2)
+					que.offer(new int[] { i, j });
+			}
 
-		int cnt = 0;
 		while (!que.isEmpty()) {
 			int[] cur = que.poll();
-			cnt++;
 
 			for (int d = 0; d < 4; d++) {
 				if (copyMap[cur[0] + dx[d]][cur[1] + dy[d]] == 0) {
