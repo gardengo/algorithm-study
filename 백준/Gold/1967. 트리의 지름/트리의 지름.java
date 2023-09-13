95fcec607d494f30b8ba9567269faf3a28e056ed
GardenGo <7704wjddnjs@gmail.com>
2023-09-13 23:34:37 +0900

    [Gold IV] Title: 트리의 지름, Time: 1100 ms, Memory: 20284 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, answer;
+	static ArrayList<ArrayList<int[]>> graph;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		graph = new ArrayList<ArrayList<int[]>>();
+		for (int i = 0; i <= n; i++)
+			graph.add(new ArrayList<int[]>());
+
+		for (int i = 0; i < n - 1; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			graph.get(start).add(new int[] { end, cost });
+		}
+
+		answer = 0;
+		for (int i = 1; i <= n; i++)
+			dfs(i);
+		System.out.println(answer);
+	}
+
+	private static int dfs(int i) {
+		int max1 = 0, max2 = 0;
+
+		ArrayList<int[]> list = graph.get(i);
+		for (int j = 0; j < list.size(); j++) {
+			int next = list.get(j)[0];
+			int cost = list.get(j)[1];
+
+			int sum = dfs(next) + cost;
+			if (sum > max1) {
+				max2 = max1;
+				max1 = sum;
+			} else if (sum > max2) {
+				max2 = sum;
+			}
+		}
+
+		answer = max1 + max2 > answer ? max1 + max2 : answer;
+		return max1;
+	}
+}
