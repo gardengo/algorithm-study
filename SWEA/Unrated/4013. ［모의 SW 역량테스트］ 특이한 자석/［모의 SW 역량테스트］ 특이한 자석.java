f5f9ea5918d7bbce35e08a1e18ae157a4a1fb5d6
GardenGo <7704wjddnjs@gmail.com>
2023-10-11 15:28:44 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 124 ms, Memory: 20,832 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -10,60 +10,47 @@ public class Solution {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine().trim());
+		int T = Integer.parseInt(br.readLine());
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int K = Integer.parseInt(br.readLine().trim());
+		for (int test = 1; test <= T; test++) {
 			magnet = new ArrayList<LinkedList<Integer>>();
-			for (int i = 0; i <= 4; i++)
+			int K = Integer.parseInt(br.readLine());
+			for (int i = 0; i < 4; i++) {
 				magnet.add(new LinkedList<Integer>());
-
-			for (int i = 1; i <= 4; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine().trim());
-				while (st.hasMoreTokens())
-					magnet.get(i).add(Integer.parseInt(st.nextToken())); // 0:N극, 1:S극
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < 8; j++)
+					magnet.get(i).add(Integer.parseInt(st.nextToken()));
 			}
-
-			boolean[] visited = new boolean[5];
 			for (int i = 0; i < K; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine().trim());
-				int num = Integer.parseInt(st.nextToken());
-				int dir = Integer.parseInt(st.nextToken()); // 1:시계방향, -1:반시계방향
-				rotate(num, dir, visited);
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int idx = Integer.parseInt(st.nextToken()) - 1;
+				int dir = Integer.parseInt(st.nextToken());
+				boolean[] visited = new boolean[4];
+				rotate(idx, dir, visited);
 			}
-
 			int answer = 0;
-			for (int i = 1; i <= 4; i++) {
-				if (magnet.get(i).get(0) == 1)
-					answer += Math.pow(2, i - 1);
-			}
-			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+			for (int i = 0; i < 4; i++)
+				answer += magnet.get(i).get(0) << i;
+			sb.append("#").append(test).append(" ").append(answer).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	private static void rotate(int num, int dir, boolean[] visited) {
-		int curR, curL, leftR, rightL;
-		visited[num] = true;
-		if (num != 1 && !visited[num - 1]) {
-			curL = magnet.get(num).get(6);
-			leftR = magnet.get(num - 1).get(2);
-			if (curL != leftR)
-				rotate(num - 1, -dir, visited);
+	public static void rotate(int idx, int dir, boolean[] visited) {
+		visited[idx] = true;
+		if (idx > 0) {
+			if (!visited[idx - 1] && magnet.get(idx).get(6) != magnet.get(idx - 1).get(2))
+				rotate(idx - 1, -dir, visited);
 		}
-		if (num != 4 && !visited[num + 1]) {
-			curR = magnet.get(num).get(2);
-			rightL = magnet.get(num + 1).get(6);
-			if (curR != rightL)
-				rotate(num + 1, -dir, visited);
+		if (idx < 3) {
+			if (!visited[idx + 1] && magnet.get(idx).get(2) != magnet.get(idx + 1).get(6))
+				rotate(idx + 1, -dir, visited);
 		}
-		visited[num] = false;
-		if (dir == 1) { // 시계방향 회전
-			int last = magnet.get(num).remove(7);
-			magnet.get(num).add(0, last);
-		} else { // 반시계방향 회전
-			int first = magnet.get(num).remove(0);
-			magnet.get(num).add(first);
-		}
+
+		if (dir == 1)
+			magnet.get(idx).addFirst(magnet.get(idx).removeLast());
+		else
+			magnet.get(idx).addLast(magnet.get(idx).removeFirst());
 	}
+
 }
