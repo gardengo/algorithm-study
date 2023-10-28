e9a1e51f210b9c962e8e0691f4c6507177645947
GardenGo <7704wjddnjs@gmail.com>
2023-10-28 11:09:57 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 103 ms, Memory: 18,384 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -2,42 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static boolean[] visited;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
+		int total = (1 << 10) - 1; // 모든 숫자가 등장 했을 때(1023)
 
 		for (int test_case = 1; test_case <= T; test_case++) {
-			visited = new boolean[10];
 			int N = Integer.parseInt(br.readLine());
-			checkNum(N);
+			int visited = 0;
 
-			int cur = N;
-			while (!checkVisited()) {
-				cur += N;
-				checkNum(cur);
+			int cnt = 1;
+			while (true) {
+				char[] arr = String.valueOf(N * cnt).toCharArray(); // 문자 배열로 변환
+				for (char ch : arr) {
+					int num = ch - '0';
+					visited = visited | (1 << num);
+				}
+				if (visited == total) // 일치하면 종료
+					break;
+				cnt++;
 			}
-			sb.append("#").append(test_case).append(" ").append(cur).append("\n"); 
+
+			sb.append("#").append(test_case).append(" ").append(N * cnt).append("\n");
 		}
 
 		System.out.println(sb);
-	}
-
-	public static void checkNum(int x) {
-		while (x != 0) {
-			visited[x - x / 10 * 10] = true;
-			x = x / 10;
-		}
-	}
-
-	public static boolean checkVisited() {
-		boolean result = true;
-		for (int i = 0; i < 10; i++)
-			if (!visited[i])
-				result = false;
-		return result;
 	}
 
 }
