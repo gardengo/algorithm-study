ccaf413109565dc0f21dee9432062d7e814bf01e
GardenGo <7704wjddnjs@gmail.com>
2024-03-15 10:59:25 +0900

    [Gold I] Title: 수영장 만들기, Time: 480 ms, Memory: 218480 KB -BaekjoonHub

--- 백준/Gold/1113. 수영장 만들기/수영장 만들기.java
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.java
@@ -11,13 +11,11 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] ground = new int[N][M];
         int[][] water = new int[N][M];
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
                 int h = str.charAt(j) - '0';
-                ground[i][j] = h;
                 water[i][j] = h;
             }
         }
@@ -25,70 +23,61 @@ public class Main {
         Queue<int[]> que = new ArrayDeque<>();
         Queue<int[]> list = new ArrayDeque<>();
         boolean[][] visited = new boolean[N][M];
-        boolean finish = true;
-        int startH = 0;
-        int maxH = 10;
+        boolean overflow = false;
+        int answer = 0;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-
-                que.add(new int[]{i, j, water[i][j]});
-                for (int k = 0; k < N; k++)
-                    Arrays.fill(visited[k], false);
-                finish = true;
-                startH = water[i][j];
-                maxH = 10;
-
-                bfs:
-                while (!que.isEmpty()) {
-                    int[] cur = que.poll();
-                    int cx = cur[0];
-                    int cy = cur[1];
-
-                    if (cur[2] <= startH) {
-                        list.offer(new int[]{cx, cy});
-                    } else {
-                        maxH = Math.min(maxH, cur[2]);
+        for (int k = 2; k <= 9; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (water[i][j] != k - 1)
                         continue;
+                    que.offer(new int[]{i, j, water[i][j]});
+                    list.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                    overflow = false;
+
+                    bfs:
+                    while (!que.isEmpty()) {
+                        int[] cur = que.poll();
+                        int cx = cur[0];
+                        int cy = cur[1];
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = cx + dx[d];
+                            int ny = cy + dy[d];
+
+                            if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                                overflow = true;
+                                break bfs;
+                            }
+
+                            if (water[nx][ny] > k - 1)
+                                continue;
+
+                            if (!visited[nx][ny]) {
+                                visited[nx][ny] = true;
+                                que.offer(new int[]{nx, ny, water[nx][ny]});
+                                list.offer(new int[]{nx, ny});
+                            }
+                        }
                     }
 
-                    for (int d = 0; d < 4; d++) {
-                        int nx = cx + dx[d];
-                        int ny = cy + dy[d];
-
-                        if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                            finish = false;
-                            break bfs;
-                        }
-
-                        if (!visited[nx][ny]) {
-                            visited[nx][ny] = true;
-                            que.offer(new int[]{nx, ny, water[nx][ny]});
+                    if (!overflow) {
+                        while (!list.isEmpty()) {
+                            int[] cur = list.poll();
+                            water[cur[0]][cur[1]] = k;
+                            answer++;
                         }
                     }
-                }
 
-                if (!finish) {
                     que.clear();
                     list.clear();
-                }
-
-                while (!list.isEmpty()) {
-                    int[] cur = list.poll();
-                    int cx = cur[0];
-                    int cy = cur[1];
-
-                    water[cx][cy] = maxH;
+                    for (int t = 0; t < N; t++)
+                        Arrays.fill(visited[t], false);
                 }
             }
         }
 
-        int answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                answer += water[i][j] - ground[i][j];
-            }
-        }
         System.out.println(answer);
     }
 }
