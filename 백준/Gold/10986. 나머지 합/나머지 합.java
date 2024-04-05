7fd7dc56cd0e948c872128420c09ac4e0b5a8af2
GardenGo <7704wjddnjs@gmail.com>
2024-04-05 14:11:00 +0900

    [Gold III] Title: 나머지 합, Time: 460 ms, Memory: 160144 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -9,34 +9,20 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N + 1];
         int[] cnt = new int[M];
         int sum = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken()) % M;
-            sum = (sum + arr[i]) % M;
+            int num = Integer.parseInt(st.nextToken()) % M;
+            sum = (sum + num) % M;
             cnt[sum]++;
         }
 
-        long answer = 0;
-        for (int i = 1; i <= N; i++) {
-            answer += cnt[0];
-            cnt[arr[i]]--;
-            moveArr(cnt, arr[i], M);
+        long answer = cnt[0];
+        for (int i = 0; i < M; i++) {
+            answer += (long) cnt[i] * (cnt[i] - 1) / 2;
         }
 
         System.out.println(answer);
-    }
-
-    private static void moveArr(int[] cnt, int n, int M) {
-        int[] tmp = new int[n];
-        for (int i = 0; i < n; i++)
-            tmp[i] = cnt[i];
-
-        for (int i = 0; i < M - n; i++)
-            cnt[i] = cnt[i + n];
-        for (int i = 0; i < n; i++)
-            cnt[i + M - n] = tmp[i];
     }
 }
