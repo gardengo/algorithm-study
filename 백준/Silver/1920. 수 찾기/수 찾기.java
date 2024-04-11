9c2c724e62573d0998ede744a15898e735a64da3
GardenGo <7704wjddnjs@gmail.com>
2024-04-11 12:17:12 +0900

    [Silver IV] Title: 수 찾기, Time: 504 ms, Memory: 61540 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,33 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            set.add(Integer.parseInt(st.nextToken()));
 
-		// 자연수 N과 N개의 정수를 입력받아 set에 저장한다
-		int N = Integer.parseInt(br.readLine());
-		HashSet<Integer> set = new HashSet<Integer>();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++)
-			set.add(Integer.parseInt(st.nextToken()));
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
 
-		// 자연수 M과 M개의 정수를 입력받는다
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			if (set.contains(Integer.parseInt(st.nextToken()))) {
-				sb.append(1);
-			} else {
-				sb.append(0);
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
-
+        System.out.println(sb);
+    }
 }
