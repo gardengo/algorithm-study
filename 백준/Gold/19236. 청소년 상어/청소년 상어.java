a4b40052489e656a6f3d51706a488a20d13d31be
GardenGo <7704wjddnjs@gmail.com>
2024-05-12 04:53:29 +0900

    [Gold II] Title: 청소년 상어, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.java
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.java
@@ -19,9 +19,8 @@ public class Main {
             }
         }
 
-        answer = 0;
-        answer += map[0][0][0];
-        map[0][0][0] = 17; // 상어
+        answer = map[0][0][0];
+        map[0][0][0] = -1;
         moveFish(map);
         moveShark(map, 0, 0, answer);
 
@@ -29,63 +28,44 @@ public class Main {
     }
 
     public static void moveFish(int[][][] map) {
-        // 1번 물고기부터 16번 물고기까지 찾아보자!
-        int cur = 1;
-        while (cur <= 16) {
+        for (int a = 1; a <= 16; a++) {
             int x = -1;
             int y = -1;
-            int dir = -1;
+            int b = -1;
 
-            // 물고기 위치 찾기
             search:
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) {
-                    if (map[i][j][0] == cur) {
+                    if (map[i][j][0] == a) {
                         x = i;
                         y = j;
-                        dir = map[i][j][1];
+                        b = map[i][j][1];
                         break search;
                     }
                 }
             }
-            // 물고기를 못찾았으면 이미 잡아먹혀서 없는 것
-            if (x == -1) {
-                cur++;
+            if (x == -1)
                 continue;
-            }
 
-            int cnt = 0;
-            // 물고기 이동하기
-            while (true) {
-                // 이동할 수 없으면 이동 x
-                dir = dir % 8;
-                cnt++;
-                if (cnt > 8)
-                    break;
+            for (int d = 0; d < 8; d++) {
+                int dir = (b + d) % 8;
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
-                // 맵 끝이면 방향 바꾸기
-                if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4) {
-                    dir++;
+
+                if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4)
                     continue;
-                }
-                // 상어 만나면 방향 바꾸기
-                if (map[nx][ny][0] == 17) {
-                    dir++;
+                if (map[nx][ny][0] == -1)
                     continue;
-                }
-                // 이동 방향에 다른 물고기가 있으면 swap
-                if (map[nx][ny][0] != 0) {
-                    int tmp0 = map[nx][ny][0];
-                    int tmp1 = map[nx][ny][1];
+
+                if (map[nx][ny][0] > 0) {
+                    int tmpx = map[nx][ny][0];
+                    int tmpy = map[nx][ny][1];
                     map[nx][ny][0] = map[x][y][0];
                     map[nx][ny][1] = dir;
-                    map[x][y][0] = tmp0;
-                    map[x][y][1] = tmp1;
+                    map[x][y][0] = tmpx;
+                    map[x][y][1] = tmpy;
                     break;
-                }
-                // 아무것도 없으면 그냥 이동
-                if (map[nx][ny][0] == 0) {
+                } else {
                     map[nx][ny][0] = map[x][y][0];
                     map[nx][ny][1] = dir;
                     map[x][y][0] = 0;
@@ -93,36 +73,33 @@ public class Main {
                     break;
                 }
             }
-            cur++;
         }
     }
 
     public static void moveShark(int[][][] map, int sx, int sy, int sum) {
         int dir = map[sx][sy][1];
-        for (int r = 1; r < 4; r++) {
+        for (int r = 1; r <= 3; r++) {
             int nx = sx + dx[dir] * r;
             int ny = sy + dy[dir] * r;
-            // 맵 끝이면 이동 불가
+
             if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4)
                 break;
-            // 물고기가 없으면 이동 불가
             if (map[nx][ny][0] == 0)
                 continue;
-
-            // 이동, 물고기 잡아먹기!
+            
             int[][][] copyMap = new int[4][4][2];
             for (int i = 0; i < 4; i++)
                 for (int j = 0; j < 4; j++)
                     for (int k = 0; k < 2; k++)
                         copyMap[i][j][k] = map[i][j][k];
 
-            int size = copyMap[nx][ny][0];
-            copyMap[nx][ny][0] = 17;
+            int cnt = copyMap[nx][ny][0];
+            copyMap[nx][ny][0] = -1;
             copyMap[sx][sy][0] = 0;
             copyMap[sx][sy][1] = 0;
 
             moveFish(copyMap);
-            moveShark(copyMap, nx, ny, sum + size);
+            moveShark(copyMap, nx, ny, sum + cnt);
         }
         answer = Math.max(answer, sum);
     }
